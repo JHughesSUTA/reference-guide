9d9b1c3fad66b98af46e871ca57d8a01416b5822
James Hughes <james.a.hughes@gmail.com>
2026-02-03 15:47:54 -0600

    GlideUser demo

--- ServiceNow Scripting/GlideUser/GlideUserNotes.js
+++ ServiceNow Scripting/GlideUser/GlideUserNotes.js
@@ -6,13 +6,35 @@ if (!hasITIL) {
 }
 
 // properties and methods
-firstName
-lastName
-userID
-userName
-getClientData()
-getFullName()
-hasRole()
-hasRoleExactly()
-hasRoleFromList()
-hasRoles()
+firstName;
+lastName;
+userID;
+userName;
+getClientData();
+getFullName();
+hasRole();
+hasRoleExactly();
+hasRoleFromList();
+hasRoles();
+
+// DEMO
+// console.dir(g_user) in console to see all the properties
+
+// alert 'hello firstName lastName. Your user id is: xxxxxxx' using glideUser in the js executer
+alert(
+  "Hello " +
+    g_user.firstName +
+    "" +
+    g_user.lastName +
+    ". Your user ID is: " +
+    g_user.userID,
+);
+// do the same but using getFullName shortcut
+
+alert("Do you have any roles? " + g_user.hasRoles());
+
+alert("Do you have the ITIL role?" + g_user.hasRole("itil"));
+
+alert("Your username is: " + g_user.userName);
+
+
